bca7687ae6c270b9b360b053103f092bc8c8035b
bertRC <sithalbert@ya.ru>
2019-09-22 18:41:22 +0300

    Реализован класс-сервис TaxiFareService

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        TaxiFareService service = new TaxiFareService();
+        int cost = service.taxiFare(100);
+        System.out.println(cost);
+    }
+}
---
+++ src/main/java/TaxiFareService.java
@@ -0,0 +1,19 @@
+public class TaxiFareService {
+    int taxiFare(int distance) {
+        int boardingCost = 60;
+        int kilometerCost = 20;
+        int minimalCostForDiscount = 1000;
+        int discountPercent = 5;
+        int maximumDiscount = 100;
+
+        int totalCost = boardingCost + distance * kilometerCost;
+        if (totalCost > minimalCostForDiscount) {
+            int discount = totalCost * discountPercent / 100;
+            if (discount > maximumDiscount) {
+                discount = maximumDiscount;
+            }
+            totalCost = totalCost - discount;
+        }
+        return totalCost;
+    }
+}
